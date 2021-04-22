4c5950f982207bd153e492e60416c0bba97ffe68
sentyaanko <sentyaanko>
2021-04-22 14:51:35 +0900

    Add Gameplay Modules.

--- Project/Source/GASHandsOn/GASHandsOn.Build.cs
+++ Project/Source/GASHandsOn/GASHandsOn.Build.cs
@@ -9,5 +9,7 @@ public class GASHandsOn : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
