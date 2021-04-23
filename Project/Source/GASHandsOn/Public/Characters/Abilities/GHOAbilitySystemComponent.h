72a6ae5dc95911e37ffaf2a1fbdab43967675ffd
sentyaanko <sentyaanko@gmail.com>
2021-04-23 19:18:28 +0900

    Add ASC source files.

---
+++ Project/Source/GASHandsOn/Public/Characters/Abilities/GHOAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// (C) Sentya Anko 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "GHOAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASHANDSON_API UGHOAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Project/Source/GASHandsOn/Public/Characters/Abilities/GHOAttributeSetBase.h
@@ -0,0 +1,17 @@
+// (C) Sentya Anko 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "GHOAttributeSetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASHANDSON_API UGHOAttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
