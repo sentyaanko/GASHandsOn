9f058d17e52acca1f3687816fe07f34d24eab6be
sentyaanko <sentyaanko@gmail.com>
2021-04-26 19:27:01 +0900

    Add GameplayAbility base class.

---
+++ Project/Source/GASHandsOn/Private/Characters/Abilities/GHOGameplayAbility.cpp
@@ -0,0 +1,8 @@
+// (C) Sentya Anko 2021
+
+
+#include "Characters/Abilities/GHOGameplayAbility.h"
+
+UGHOGameplayAbility::UGHOGameplayAbility()
+{
+}
---
+++ Project/Source/GASHandsOn/Public/Characters/Abilities/GHOGameplayAbility.h
@@ -0,0 +1,19 @@
+// (C) Sentya Anko 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GHOGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASHANDSON_API UGHOGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UGHOGameplayAbility();
+};
