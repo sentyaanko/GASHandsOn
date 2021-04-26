8cb9d9de434007527a03ac3976362db3ca575f00
sentyaanko <sentyaanko@gmail.com>
2021-04-26 21:32:27 +0900

    Add CharacterAbilities to CharacterBase.

--- Project/Source/GASHandsOn/Public/Characters/Abilities/GHOGameplayAbility.h
+++ Project/Source/GASHandsOn/Public/Characters/Abilities/GHOGameplayAbility.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
+#include "GASHandsOn.h"
 #include "GHOGameplayAbility.generated.h"
 
 /**
@@ -13,7 +14,18 @@ UCLASS()
 class GASHANDSON_API UGHOGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
-	
+
 public:
 	UGHOGameplayAbility();
+
+public:
+	// Abilities with this set will automatically activate when the input is pressed
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Ability")
+	EGHOAbilityInputID AbilityInputID = EGHOAbilityInputID::None;
+
+	// Value to associate an ability with an slot without tying it to an automatically activated input.
+	// Passive abilities won't be tied to an input so we need a way to generically associate abilities with slots.
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Ability")
+	EGHOAbilityInputID AbilityID = EGHOAbilityInputID::None;
+
 };
--- Project/Source/GASHandsOn/Public/Characters/GHOCharacterBase.h
+++ Project/Source/GASHandsOn/Public/Characters/GHOCharacterBase.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
+#include "GASHandsOn.h"
 #include "GHOCharacterBase.generated.h"
 
 UCLASS()
@@ -41,6 +42,9 @@ public:
 	// End of IAbilitySystemInterface interface
 
 
+public:
+	void Die();
+
 protected:
 	// Initialize the Character's attributes. Must run on Server but we run it on Client too
 	// so that we don't have to wait. The Server's replication to the Client won't matter since
@@ -50,7 +54,24 @@ protected:
 	//
 	void AddStartupEffects();
 
+protected:
 	// Grant abilities on the Server. The Ability Specs will be replicated to the owning client.
 	void AddCharacterAbilities();
 
+private:
+	// Removes all CharacterAbilities. Can only be called by the Server. Removing on the Server will remove from Client too.
+	void RemoveCharacterAbilities();
+
+	// Switch on AbilityID to return individual ability levels. Hardcoded to 1 for every ability in this project.
+	UFUNCTION(BlueprintCallable, Category = "GASHandsOn|GDCharacter")
+	virtual int32 GetAbilityLevel(EGHOAbilityInputID AbilityID) const;
+
+
+protected:
+	// Default abilities for this Character. These will be removed on Character death and regiven if Character respawns.
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GASHandsOn|Abilities")
+	TArray<TSubclassOf<class UGHOGameplayAbility>> CharacterAbilities;
+
+private:
+	bool bCharacterAbilitiesGiven = false;
 };
