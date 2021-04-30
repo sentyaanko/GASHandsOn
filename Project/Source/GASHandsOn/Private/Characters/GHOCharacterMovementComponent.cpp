ab14cd069e8a7974f09ddca0385d40eead9b3cbe
sentyaanko <sentyaanko@gmail.com>
2021-04-30 23:36:43 +0900

    Implement CharacterMovementComponent.

--- Project/Source/GASHandsOn/Private/Characters/GHOCharacterMovementComponent.cpp
+++ Project/Source/GASHandsOn/Private/Characters/GHOCharacterMovementComponent.cpp
@@ -2,4 +2,196 @@
 
 
 #include "Characters/GHOCharacterMovementComponent.h"
+#include "Characters/GHOCharacterBase.h"
 
+UGHOCharacterMovementComponent::UGHOCharacterMovementComponent()
+{
+	NetworkMaxSmoothUpdateDistance = 92.f;
+	NetworkNoSmoothUpdateDistance = 140.f;
+	AimDownSightsSpeedMultiplier = 0.5f;
+	MoveFlag = EGHOMoveFlag::MF_None;
+}
+
+FNetworkPredictionData_Client * UGHOCharacterMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != NULL);
+
+	if (!ClientPredictionData)
+	{
+		UGHOCharacterMovementComponent* MutableThis = const_cast<UGHOCharacterMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FGHONetworkPredictionData_Client(*this);
+#if 0
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+#endif
+	}
+
+	return ClientPredictionData;
+}
+
+float UGHOCharacterMovementComponent::GetMaxSpeed() const
+{
+	AGHOCharacterBase* Owner = Cast<AGHOCharacterBase>(GetOwner());
+	if (!Owner)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s() No Owner"), *FString(__FUNCTION__));
+		return Super::GetMaxSpeed();
+	}
+
+#if 0
+	if (!Owner->IsAlive())
+	{
+		return 0.0f;
+	}
+
+	if (Owner->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Debuff.Stun"))))
+	{
+	return 0.0f;
+	}
+#else
+//TODO :After merge Health implement.
+//if(!Owner->IsMovable())
+//{
+//    return 0.f;
+//}
+
+
+#endif
+#if 0
+#if 0
+if (RequestToStartSprinting)
+{
+	return Owner->GetMoveSpeed() * SprintSpeedMultiplier;
+}
+#endif
+if (RequestToStartADS)
+{
+	return Owner->GetMoveSpeed() * AimDownSightsSpeedMultiplier;
+}
+
+return Owner->GetMoveSpeed();
+#else
+	//TODO: Fix after MoveSpeed Attribute implemented.
+	//return Owner->GetMoveSpeed() * GetSpeedMultiplier(MoveFlag);
+return 600.f * GetSpeedMultiplier(MoveFlag);
+#endif
+}
+
+void UGHOCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+
+	//The Flags parameter contains the compressed input flags that are stored in the saved move.
+	//UpdateFromCompressed flags simply copies the flags from the saved move into the movement component.
+	//It basically just resets the movement component to the state when the move was made so it can simulate from there.
+#if 0
+	RequestToStartSprinting = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+
+	RequestToStartADS = (Flags & FSavedMove_Character::FLAG_Custom_1) != 0;
+#else
+	if (Flags & FSavedMove_Character::FLAG_Custom_1)
+		StartAimDownSights();
+	else
+		StopAimDownSights();
+#endif
+}
+
+void UGHOCharacterMovementComponent::StartAimDownSights()
+{
+	EnumAddFlags(MoveFlag, EGHOMoveFlag::MF_AimDownSights);
+}
+
+void UGHOCharacterMovementComponent::StopAimDownSights()
+{
+	EnumRemoveFlags(MoveFlag, EGHOMoveFlag::MF_AimDownSights);
+}
+
+float UGHOCharacterMovementComponent::GetSpeedMultiplier(const EGHOMoveFlag& Flag)const
+{
+	if (EnumHasAnyFlags(Flag, EGHOMoveFlag::MF_AimDownSights))
+		return AimDownSightsSpeedMultiplier;
+	else
+		return 1.f;
+}
+
+void UGHOCharacterMovementComponent::FGHOSavedMove::Clear()
+{
+	Super::Clear();
+	MoveFlag = EGHOMoveFlag::MF_None;
+}
+
+void UGHOCharacterMovementComponent::FGHOSavedMove::SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData)
+{
+	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	if (UGHOCharacterMovementComponent* CharacterMovement = Cast<UGHOCharacterMovementComponent>(Character->GetCharacterMovement()))
+	{
+		MoveFlag = CharacterMovement->GetMoveFlag();
+	}
+}
+
+bool UGHOCharacterMovementComponent::FGHOSavedMove::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* Character, float MaxDelta) const
+{
+	//Set which moves can be combined together. This will depend on the bit flags that are used.
+#if 0
+	if (SavedRequestToStartSprinting != ((FGHOSavedMove*)&NewMove)->SavedRequestToStartSprinting)
+	{
+		return false;
+	}
+
+	if (SavedRequestToStartADS != ((FGHOSavedMove*)&NewMove)->SavedRequestToStartADS)
+	{
+		return false;
+	}
+#else
+	if (MoveFlag != ((FGHOSavedMove*)&NewMove)->MoveFlag)
+	{
+		return false;
+	}
+#endif
+	return Super::CanCombineWith(NewMove, Character, MaxDelta);
+}
+
+void UGHOCharacterMovementComponent::FGHOSavedMove::PrepMoveFor(class ACharacter* Character)
+{
+	Super::PrepMoveFor(Character);
+
+	if (UGHOCharacterMovementComponent* CharacterMovement = Cast<UGHOCharacterMovementComponent>(Character->GetCharacterMovement()))
+	{
+	}
+
+}
+
+uint8 UGHOCharacterMovementComponent::FGHOSavedMove::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+#if 0
+	if (SavedRequestToStartSprinting)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	if (SavedRequestToStartADS)
+	{
+		Result |= FLAG_Custom_1;
+	}
+#else
+	if(EnumHasAnyFlags(MoveFlag, EGHOMoveFlag::MF_AimDownSights))
+		Result |= FLAG_Custom_1;
+#endif
+	return Result;
+
+}
+
+UGHOCharacterMovementComponent::FGHONetworkPredictionData_Client::FGHONetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement)
+	:Super(ClientMovement)
+{
+
+}
+
+FSavedMovePtr UGHOCharacterMovementComponent::FGHONetworkPredictionData_Client::AllocateNewMove()
+{
+	return FSavedMovePtr(new FGHOSavedMove());
+}
--- Project/Source/GASHandsOn/Public/Characters/GHOCharacterMovementComponent.h
+++ Project/Source/GASHandsOn/Public/Characters/GHOCharacterMovementComponent.h
@@ -6,6 +6,13 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GHOCharacterMovementComponent.generated.h"
 
+enum class EGHOMoveFlag : uint8
+{
+	MF_None = 0x00,
+	MF_AimDownSights = 0x01,
+};
+ENUM_CLASS_FLAGS(EGHOMoveFlag)
+
 /**
  * 
  */
@@ -14,4 +21,116 @@ class GASHANDSON_API UGHOCharacterMovementComponent : public UCharacterMovementC
 {
 	GENERATED_BODY()
 	
+private:
+	class FGHOSavedMove : public FSavedMove_Character
+	{
+	public:
+
+		typedef FSavedMove_Character Super;
+
+		/** Clear saved move properties, so it can be re-used. */
+		///@brief Resets all saved variables.
+		virtual void Clear() override;
+
+		/** Called to set up this saved move (when initially created) to make a predictive correction. */
+		///@brief Sets up the move before sending it to the server. 
+		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData) override;
+
+		/** Returns true if this move can be combined with NewMove for replication without changing any behavior */
+		///@brief This is used to check whether or not two moves can be combined into one.
+		///Basically you just check to make sure that the saved variables are the same.
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* Character, float MaxDelta) const override;
+
+		/** Called before ClientUpdatePosition uses this SavedMove to make a predictive correction	 */
+		///@brief Sets variables on character movement component before making a predictive correction.
+		virtual void PrepMoveFor(class ACharacter* Character) override;
+
+		/** Returns a byte containing encoded special movement information (jumping, crouching, etc.)	 */
+		///@brief Store input commands in the compressed flags.
+		virtual uint8 GetCompressedFlags() const override;
+
+#if 0
+		// Sprint
+		uint8 SavedRequestToStartSprinting : 1;
+
+		// Aim Down Sights
+		uint8 SavedRequestToStartADS : 1;
+#else
+		EGHOMoveFlag MoveFlag;
+#endif
+	};
+
+	class FGHONetworkPredictionData_Client : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FGHONetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		/** Allocate a new saved move. Subclasses should override this if they want to use a custom move class. */
+		///@brief Allocates a new copy of our custom saved move
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
+public:
+	UGHOCharacterMovementComponent();
+
+	// INetworkPredictionInterface interface
+public:
+	/** Unpack compressed flags from a saved move and set state accordingly. See FSavedMove_Character. */
+	/** Get prediction data for a client game. Should not be used if not running as a client. Allocates the data on demand and can be overridden to allocate a custom override if desired. Result must be a FNetworkPredictionData_Client_Character. */
+	/** @return FNetworkPredictionData_Client instance used for network prediction. */
+	virtual class FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+	// End of INetworkPredictionInterface interface
+
+	// UMovementComponent interface
+public:
+	virtual float GetMaxSpeed() const override;
+
+	// End of UMovementComponent interface
+
+	// UMovementComponent interface
+protected:
+	/** Unpack compressed flags from a saved move and set state accordingly. See FSavedMove_Character. */
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+
+	// End of UMovementComponent interface
+
+public:
+	const EGHOMoveFlag& GetMoveFlag()const { return MoveFlag; }
+
+protected:
+#if 0
+	// Sprint
+	UFUNCTION(BlueprintCallable, Category = "Sprint")
+	void StartSprinting();
+	UFUNCTION(BlueprintCallable, Category = "Sprint")
+	void StopSprinting();
+#endif
+
+	// Aim Down Sights
+	UFUNCTION(BlueprintCallable, Category = "Aim Down Sights")
+	void StartAimDownSights();
+	UFUNCTION(BlueprintCallable, Category = "Aim Down Sights")
+	void StopAimDownSights();
+
+private:
+	float GetSpeedMultiplier(const EGHOMoveFlag& Flag)const;
+
+protected:
+#if 0
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sprint")
+	float SprintSpeedMultiplier;
+#endif
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Aim Down Sights")
+	float AimDownSightsSpeedMultiplier;
+
+private:
+#if 0
+	uint8 RequestToStartSprinting : 1;
+	uint8 RequestToStartADS : 1;
+#else
+	EGHOMoveFlag MoveFlag;
+#endif
 };
