ee0bc371ea2636c55dfe7afc3d565569ab735a61
sentyaanko <sentyaanko@gmail.com>
2021-05-05 16:04:45 +0900

    Add comments and update history.

--- Project/Source/GASHandsOn/Public/Characters/Abilities/GHOGA_CharacterJump.h
+++ Project/Source/GASHandsOn/Public/Characters/Abilities/GHOGA_CharacterJump.h
@@ -21,17 +21,43 @@ public:
 
 	// UGameplayAbility interface
 public:
-	/** Returns true if this ability can be activated right now. Has no side effects */
+	/*
+	by Epic
+		Returns true if this ability can be activated right now. Has no side effects
+	和訳
+		このアビリティが今すぐに起動できる場合は true を返します。副作用はありません。
+	*/
 	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
 
-	/** Destroys instanced-per-execution abilities. Instance-per-actor abilities should 'reset'. Any active ability state tasks receive the 'OnAbilityStateInterrupted' event. Non instance abilities - what can we do? */
+	/*
+	by Epic
+		Destroys instanced-per-execution abilities. 
+		Instance-per-actor abilities should 'reset'. 
+		Any active ability state tasks receive the 'OnAbilityStateInterrupted' event. 
+		Non instance abilities - what can we do?
+	和訳
+		instanced-per-execution のアビリティを破棄する。
+		instanced-per-execution のアビリティは「リセット」されるべきである。
+		アクティブなアビリティ状態タスクは 'OnAbilityStateInterrupted' イベントを受け取ります。
+		非インスタンスアビリティ - 何ができる？
+	*/
 	virtual void CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility) override;
 
-	/** Input binding stub. */
+	/*
+	by Epic
+		Input binding stub.
+	和訳
+		入力バインディングのスタブ。
+	*/
 	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
 
 protected:
-	/** Actually activate ability, do not call this directly */
+	/*
+	by Epic
+		Actually activate ability, do not call this directly
+	和訳
+		実際にアビリティを起動する、これを直接呼び出さないでください。
+	*/
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 	// End of UGameplayAbility interface
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # GASHandsOn
 Gameplay Ability System Hands On Project.
+
+GASDocumentation で行っている各種機能を一つづつ実装していくハンズオン。
+
+
+# 更新履歴
+
+* v0.0.1
+	* UE4 のテンプレートで自動生成されたファイルの追加
+	* GameplayAbility プラグインの有効化
+	* Gameplay Modules の有効化
+	* Public/Privete フォルダの階層分け
+* v0.0.2
+	* GameInstance/GameMode/GameState/PlayerState/PlayerController などの基本的なクラスの追加
+	* Player 操作用の Character 基底クラスの用意、テンプレートから移動などの最低限の操作用コードの複製
+	* 作業用のマップをテンプレートから複製
+	* BluePrint のファイル群のフォルダ分け
+* v0.0.3
+	* AbilitySystemComponent のソース追加
+	* AttributeSet のソース追加
+	* Character/PlayerState/PlayerController などに AbilitySystemComponent/AttributeSet を設定
+	* プロジェクト用 GameplayAbility 基底クラスの追加
+* v0.0.4
+	* プロジェクト用 CharacterMovementComponent 基底クラスの追加
+* v0.0.5
+	* Character に Health の Attribute 組み込み
+	* 詳細は [./Documents/Add-CharacterHealth.md](./Documents/Add-CharacterHealth.md) を参照
+* v0.0.7
+	* Character に Jump の Ability 組み込み
+	* 詳細は [./Documents/Add-CharacterJump.md](./Documents/Add-CharacterJump.md) を参照
+* v0.0.8
+	* Character に AimDownSights の Ability 組み込み
+	* 詳細は [./Documents/Add-AimDownSights.md](./Documents/Add-AimDownSights.md) を参照
+
+-----
+おしまい。
