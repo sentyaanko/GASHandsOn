780cb14315bdfd26d1e1503b1a69c69764a6a928
sentyaanko <sentyaanko@gmail.com>
2021-08-08 17:37:06 +0900

    Add Blueprint Function Library. Add ClearTargetData() function.

---
+++ Project/Source/GASHandsOn/Private/FunctionLibrary/GHOBlueprintFunctionLibrary.cpp
@@ -0,0 +1,9 @@
+// (C) Sentya Anko 2021
+
+
+#include "FunctionLibrary/GHOBlueprintFunctionLibrary.h"
+
+void UGHOBlueprintFunctionLibrary::ClearTargetData(FGameplayAbilityTargetDataHandle& TargetData)
+{
+	TargetData.Clear();
+}
