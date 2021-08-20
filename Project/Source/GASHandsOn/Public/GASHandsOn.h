e172d941e4bd9ab54c7a6c6aeeb4c3a5a802c7ba
sentyaanko <sentyaanko@gmail.com>
2021-08-20 17:04:20 +0900

    Add Macros for debug.

--- Project/Source/GASHandsOn/Public/GASHandsOn.h
+++ Project/Source/GASHandsOn/Public/GASHandsOn.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 
+#define ACTOR_ROLE_FSTRING *(FindObject<UEnum>(ANY_PACKAGE, TEXT("ENetRole"), true)->GetNameStringByValue(GetLocalRole()))
+#define GET_ACTOR_ROLE_FSTRING(Actor) *(FindObject<UEnum>(ANY_PACKAGE, TEXT("ENetRole"), true)->GetNameStringByValue(Actor->GetLocalRole()))
 
 UENUM(BlueprintType)
 enum class EGHOAbilityInputID : uint8
