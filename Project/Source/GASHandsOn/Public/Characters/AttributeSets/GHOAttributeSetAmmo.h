0842d344784864ca668813a7f2ed594fa5362269
sentyaanko <sentyaanko@gmail.com>
2021-09-03 00:39:28 +0900

    Add include file.

--- Project/Source/GASHandsOn/Public/Characters/AttributeSets/GHOAttributeSetAmmo.h
+++ Project/Source/GASHandsOn/Public/Characters/AttributeSets/GHOAttributeSetAmmo.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "GameplayTagContainer.h"
 #include "GHOAttributeSetMacros.h"
 #include "GHOAttributeSetAmmo.generated.h"
